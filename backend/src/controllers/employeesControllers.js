a10ce1e5794662199a4428113e51bd2456bb12bf
Paola <20230523@ricaldone.edu.sv>
2025-03-11 11:25:13 -0600

    Crud empleados finalizado

---
+++ backend/src/controllers/employeesControllers.js
@@ -0,0 +1,24 @@
+const employeesControllers = {};
+
+import employees from "../models/employees.js"
+import employeesModel from "../models/employees.js"
+
+//SELECT 
+employeesControllers.getEmployees = async (req, res) => {
+    const employees = await employeesModel.find()
+    res.json(employees)
+}
+
+//INSERT
+employeesControllers.insertEmployees = async (req, res) => {
+    const {name, lastName, birthday, email, address, hireDate, password, telephone, dui, isssNumber, isVerified} = req.body;
+    const newEmployees = new employeesModel({name, lastName, birthday, email, address, hireDate, password, telephone, dui, isssNumber, isVerified})
+    await newEmployees.save()
+    res.json({message: "Employees saved"})
+}
+
+//DELETE
+employeesControllers.deleteEmployees = async (req, res) => {
+    await employeesModel.findByIdAndDelete(req.params.id)
+    res.json({message: "Employees delete"})
+}
